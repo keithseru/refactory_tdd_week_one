b865574ff505f6f91d06603af444d177833ccc5b
Keith James Seruyange <keithseruyange@gmail.com>
2026-03-04 22:08:42 +0300

    created the test for division by zero

--- calculator/test_calculator.py
+++ calculator/test_calculator.py
@@ -11,12 +11,17 @@ def test_subtract():
     result = subtract(2,3)
     assert result == -1
     
-# Test the multiplication function
+# Test 3: Test the multiplication function
 def test_multiply():
     result = multiply(2,3)
     assert result == 6
 
-# Test the division function
+# Test 4: Test the division function
 def test_divide():
     result = divide(4,2)
     assert result == 2
+    
+# Test 5: Raising an error when diving by zero
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        divide(5,0)
